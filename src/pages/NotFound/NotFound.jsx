8bae441fab7b7761300071d478fe2a0e644874d9
Simon <contact@smn.codes>
2026-02-24 12:26:05 +0100

    fix: NotFound page should do full reload if stored tenant id does not match url

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -4,15 +4,20 @@ import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import Button from '../../components/Buttons/Button';
 import { useTenant } from '@/context/TenantContext';
+import { matchesTenantFromUrl } from '@/lib/tenant';
 
 
 const NotFound = () => {
 	const navigate = useNavigate();
 	const { t } = useTranslation();
-	const { buildPath } = useTenant();
+	const { buildPath, effectiveTenantId, urlTenantId } = useTenant();
 
 	const handleBackToHome = () => {
-		navigate(buildPath());
+		if (!matchesTenantFromUrl(effectiveTenantId, urlTenantId)) {
+			window.location.href = buildPath();
+		} else {
+			navigate(buildPath());
+		}
 	};
 
 	return (
