8eb0c7a04c121d2da6923d3a8a42fb6d80771917
Simon <contact@smn.codes>
2026-06-05 12:45:25 +0200

    fix: use url tenant id always for authzen config

--- src/context/CredentialsContextProvider.tsx
+++ src/context/CredentialsContextProvider.tsx
@@ -7,7 +7,7 @@ import { useOpenID4VCIHelper } from "@/lib/services/OpenID4VCIHelper";
 import { CurrentSchema } from '@/services/WalletStateSchema';
 import { logger } from '../logger';
 import { BACKEND_URL } from '@/config';
-import { getStoredTenant } from '@/lib/tenant';
+import { getStoredTenant, getTenantFromUrlPath } from '@/lib/tenant';
 import { useHttpClient } from '@/hooks/useHttpClient';
 
 type WalletStateCredential = CurrentSchema.WalletStateCredential;
@@ -33,8 +33,8 @@ export const CredentialsContextProvider = ({ children }: React.PropsWithChildren
 		const clientConfig: AuthZENClientConfig = {
 			httpClient: httpClient,
 			baseUrl: BACKEND_URL,
-			getAuthToken: () => JSON.parse(sessionStorage.getItem('appToken')!),
-			tenantId: getStoredTenant() || 'default',
+			getAuthToken: () => api.getAppToken() ?? '',
+			tenantId: getTenantFromUrlPath() ?? 'default',
 		};
 		return AuthZENClient(clientConfig);
 	}, [httpClient]);
