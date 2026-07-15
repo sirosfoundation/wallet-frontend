9f8737f0b4cb1325eb5479a59c38ce9eb40acbb7
yitbarek <yitbarekyohannes0@gmail.com>
2026-07-15 11:51:02 +0000

    support mdoc verification for full device response

--- src/hooks/useOIDFlowSignHandler.ts
+++ src/hooks/useOIDFlowSignHandler.ts
@@ -10,6 +10,9 @@ import { buildMdocPresentationDefinition, parseIssuerSignedToMDoc } from '@/lib/
 import { detectCredentialFormat, VerifiableCredentialFormat } from 'wallet-common';
 import { MDoc } from '@auth0/mdl';
 import { LocalStorageKeystore } from '@/services/LocalStorageKeystore';
+import * as cbor from 'cbor-x'; 
+import { fromBase64Url, toBase64Url } from "../util";
+
 
 interface ProofTypeConfig {
 	key_attestations_required?: Record<string, unknown> | null;
@@ -299,13 +302,16 @@ async function createVpTokenFromMdoc(
 	if (!disclosedClaims?.length) {
 		throw new Error('disclosedClaims required for mdoc presentation');
 	}
-
-	const mdoc = parseIssuerSignedToMDoc(credentialRaw);
-	const presentationDefinition = buildMdocPresentationDefinition(
+	const deviceResponse = cbor.decode(fromBase64Url(credentialRaw));
+	const issuerSigned = deviceResponse.documents[0].issuerSigned;
+	const issuerSignedBytes = cbor.encode(issuerSigned);
+	const issuerSignedB64 = toBase64Url(issuerSignedBytes);
+	const mdoc = parseIssuerSignedToMDoc(issuerSignedB64);
+	const presentationDefinition = buildMdocPresentationDefinition(	
 		mdoc.documents[0].docType,
 		disclosedClaims ?? [],
 	);
-
+	console.log("presentation Defnition: ", presentationDefinition)
 	let deviceResponseMDoc: MDoc;
 	if (responseUri) {
 		const { deviceResponseMDoc: drm } = await keystore.generateDeviceResponse(
