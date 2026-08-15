a706edd33542e1422a30c801681caf4a95190ff1
Simon <contact@smn.codes>
2026-08-15 17:59:44 +0200

    refactor: add todo comment, move utils to utils.ts and add tests, and fix `resolveClaimLabel()` not picking the first preferred language

--- src/components/flows/PresentCredentialsFlow/index.ts
+++ src/components/flows/PresentCredentialsFlow/index.ts
@@ -1,3 +1,16 @@
 export * from './PresentCredentialsFlow';
 export * from './usePresentCredentialsFlow';
 export type * from './types';
+
+/**
+ * @todo - Refactor this directory structure
+ *
+ * It's fairly obvious that this dir is a bit of a mess,
+ * with both UI components, a hook and utils all mashed together.
+ *
+ * I'll split this up into a better structure, when we have a more
+ * generic place for `credentials` related logic.
+ *
+ * For the moment though, since this is all fairly uncomplicated
+ * and already self-contained, I'm leaving it as-is.
+ */
