e3c512356cc65068d3ea09952b767e0c252805a9
Simon <contact@smn.codes>
2026-09-01 18:46:25 +0200

    refactor: send callback url to updateAllCredentials()

--- src/hocs/NativeWrapperProvider.tsx
+++ src/hocs/NativeWrapperProvider.tsx
@@ -4,6 +4,7 @@ import keystoreEvents from "../services/keystoreEvents";
 import CredentialsContext from "@/context/CredentialsContext";
 import { prepareCredentialsForNativeWrapper } from "@/lib/native-wrapper";
 import { logger } from "@/logger";
+import { OPENID4VCI_REDIRECT_URI } from "@/config";
 
 declare global {
 	interface Window {
@@ -11,7 +12,7 @@ declare global {
 	}
 
 	interface NativeWrapper {
-		updateAllCredentials(credentials: string): void;
+		updateAllCredentials(credentials: string, callbackUrl?: string): void;
 		sendDcApiResponse(response: string, error?: string): void;
 		isKeystoreOpen(): Promise<boolean>;
 		startScanPhysicalId?(): void;
@@ -37,8 +38,14 @@ export const NativeWrapperProvider = ({
 			const registryEntries =
 				await prepareCredentialsForNativeWrapper(vcEntityList);
 
-			logger.debug("Updated native wrapper with credentials:", registryEntries);
-			window.nativeWrapper.updateAllCredentials(JSON.stringify(registryEntries));
+			logger.debug("Updated native wrapper with credentials and callback URL", {
+				registryEntries,
+				callbackUrl: OPENID4VCI_REDIRECT_URI,
+			});
+			window.nativeWrapper.updateAllCredentials(
+				JSON.stringify(registryEntries),
+				OPENID4VCI_REDIRECT_URI,
+			);
 		})();
 	}, [vcEntityList]);
 
