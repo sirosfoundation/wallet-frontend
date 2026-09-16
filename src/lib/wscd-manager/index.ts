7c242b9992fe755312c1774cc90b7224709afbfb
Simon <contact@smn.codes>
2026-09-16 17:43:20 +0200

    fix: remove broken import

--- src/lib/wscd-manager/index.ts
+++ src/lib/wscd-manager/index.ts
@@ -3,7 +3,6 @@ export * from './WscdManagerClient';
 export * from './hosts/WscdManagerInPageHost';
 export * from './hosts/WscdManagerWorkerHost';
 export * from './hosts/WscdManagerNativeWrapperHost';
-export * from './hosts/WscdManagerWalletCompanionHost';
 
 export * from './resources';
 export * from './types';
